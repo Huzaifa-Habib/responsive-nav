1f919b66b450b57a8b4c8c1de02cff6bab221435
Huzaifa Habib <101453901+cocobutte@users.noreply.github.com>
2022-06-28 21:44:15 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+function get(){
+var userName=document.getElementById("name").value;
+console.log("Welcome " + userName);
+
+var mail=document.getElementById("mail").value;
+console.log("Your Email is " + mail)
+
+}
